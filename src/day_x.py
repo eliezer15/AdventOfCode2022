5ad6c803b53b5add7c142cd7c8f23fa1e7a06704
Eliezer Encarnacion <eliezerencarnacion@gmail.com>
2022-12-08 20:42:20 -0500

    Add a template for each new day

---
+++ src/day_x.py
@@ -0,0 +1,16 @@
+def part_one():
+    pass
+
+def part_two():
+    pass
+
+def get_input_lines():
+    with open('input.txt', 'r') as file:
+        return file.readlines()
+
+def main():
+    part_one()
+    part_two()
+
+if __name__ == '__main__':
+    main()
